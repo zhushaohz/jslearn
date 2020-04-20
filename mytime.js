fef1c9667b76cf029080877b6bf36f99f6849972
zhushaohz <zhushaohz@163.com>
2020-04-20 10:12:19 +0800

    调试

--- mytime.js
+++ mytime.js
@@ -1,5 +1,9 @@
 function h(){
 	console.log('哈哈哈');
-	$notify("Title", "Subtitle", '哈哈哈');
+	var title = 'dd';
+	var subtitle = 'ff';
+	var body = 'ed';
+	//$notify("Title", "Subtitle", '哈哈哈');
+	$notification.post(title, subtitle, body)
 }
 h();
